7c23a1eddfc1ee27829d90b09c6fd3cc6490f831
Vu Duyen <vuduyen145@gmail.com>
2023-05-28 21:38:04 +0700

    topic 3 aws

---
+++ src/FileActionsDemo.DbMigrator/FileActionsDemoDbMigratorModule.cs
@@ -0,0 +1,15 @@
+﻿using FileActionsDemo.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace FileActionsDemo.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(FileActionsDemoEntityFrameworkCoreModule),
+    typeof(FileActionsDemoApplicationContractsModule)
+    )]
+public class FileActionsDemoDbMigratorModule : AbpModule
+{
+
+}
---
+++ src/FileActionsDemo.Web/FileActionsDemoWebModule.cs
@@ -0,0 +1,215 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using FileActionsDemo.EntityFrameworkCore;
+using FileActionsDemo.Localization;
+using FileActionsDemo.MultiTenancy;
+using FileActionsDemo.Web.Menus;
+using Microsoft.OpenApi.Models;
+using OpenIddict.Validation.AspNetCore;
+using Volo.Abp;
+using Volo.Abp.Account.Web;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.Localization;
+using Volo.Abp.AspNetCore.Mvc.UI;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.LeptonXLite;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.LeptonXLite.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity.Web;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement.Web;
+using Volo.Abp.SettingManagement.Web;
+using Volo.Abp.Swashbuckle;
+using Volo.Abp.TenantManagement.Web;
+using Volo.Abp.UI.Navigation.Urls;
+using Volo.Abp.UI;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.VirtualFileSystem;
+
+namespace FileActionsDemo.Web;
+
+[DependsOn(
+    typeof(FileActionsDemoHttpApiModule),
+    typeof(FileActionsDemoApplicationModule),
+    typeof(FileActionsDemoEntityFrameworkCoreModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpIdentityWebModule),
+    typeof(AbpSettingManagementWebModule),
+    typeof(AbpAccountWebOpenIddictModule),
+    typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
+    typeof(AbpTenantManagementWebModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpSwashbuckleModule)
+    )]
+public class FileActionsDemoWebModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
+        {
+            options.AddAssemblyResource(
+                typeof(FileActionsDemoResource),
+                typeof(FileActionsDemoDomainModule).Assembly,
+                typeof(FileActionsDemoDomainSharedModule).Assembly,
+                typeof(FileActionsDemoApplicationModule).Assembly,
+                typeof(FileActionsDemoApplicationContractsModule).Assembly,
+                typeof(FileActionsDemoWebModule).Assembly
+            );
+        });
+
+        PreConfigure<OpenIddictBuilder>(builder =>
+        {
+            builder.AddValidation(options =>
+            {
+                options.AddAudiences("FileActionsDemo");
+                options.UseLocalServer();
+                options.UseAspNetCore();
+            });
+        });
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        ConfigureAuthentication(context);
+        ConfigureUrls(configuration);
+        ConfigureBundles();
+        ConfigureAutoMapper();
+        ConfigureVirtualFileSystem(hostingEnvironment);
+        ConfigureNavigationServices();
+        ConfigureAutoApiControllers();
+        ConfigureSwaggerServices(context.Services);
+    }
+
+    private void ConfigureAuthentication(ServiceConfigurationContext context)
+    {
+        context.Services.ForwardIdentityAuthenticationForBearer(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+    }
+
+    private void ConfigureUrls(IConfiguration configuration)
+    {
+        Configure<AppUrlOptions>(options =>
+        {
+            options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+        });
+    }
+
+    private void ConfigureBundles()
+    {
+        Configure<AbpBundlingOptions>(options =>
+        {
+            options.StyleBundles.Configure(
+                LeptonXLiteThemeBundles.Styles.Global,
+                bundle =>
+                {
+                    bundle.AddFiles("/global-styles.css");
+                }
+            );
+        });
+    }
+
+    private void ConfigureAutoMapper()
+    {
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<FileActionsDemoWebModule>();
+        });
+    }
+
+    private void ConfigureVirtualFileSystem(IWebHostEnvironment hostingEnvironment)
+    {
+        if (hostingEnvironment.IsDevelopment())
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.ReplaceEmbeddedByPhysical<FileActionsDemoDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}FileActionsDemo.Domain.Shared"));
+                options.FileSets.ReplaceEmbeddedByPhysical<FileActionsDemoDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}FileActionsDemo.Domain"));
+                options.FileSets.ReplaceEmbeddedByPhysical<FileActionsDemoApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}FileActionsDemo.Application.Contracts"));
+                options.FileSets.ReplaceEmbeddedByPhysical<FileActionsDemoApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}FileActionsDemo.Application"));
+                options.FileSets.ReplaceEmbeddedByPhysical<FileActionsDemoWebModule>(hostingEnvironment.ContentRootPath);
+            });
+        }
+    }
+
+    private void ConfigureNavigationServices()
+    {
+        Configure<AbpNavigationOptions>(options =>
+        {
+            options.MenuContributors.Add(new FileActionsDemoMenuContributor());
+        });
+    }
+
+    private void ConfigureAutoApiControllers()
+    {
+        Configure<AbpAspNetCoreMvcOptions>(options =>
+        {
+            options.ConventionalControllers.Create(typeof(FileActionsDemoApplicationModule).Assembly);
+        });
+    }
+
+    private void ConfigureSwaggerServices(IServiceCollection services)
+    {
+        services.AddAbpSwaggerGen(
+            options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "FileActionsDemo API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            }
+        );
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAbpRequestLocalization();
+
+        if (!env.IsDevelopment())
+        {
+            app.UseErrorPage();
+        }
+
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAbpOpenIddictValidation();
+
+        if (MultiTenancyConsts.IsEnabled)
+        {
+            app.UseMultiTenancy();
+        }
+
+        app.UseUnitOfWork();
+        app.UseAuthorization();
+        app.UseSwagger();
+        app.UseAbpSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "FileActionsDemo API");
+        });
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
+    }
+}
